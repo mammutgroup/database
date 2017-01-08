362f1b31c5f25223e57eaac6c891135fd93bf5f8
sayadaazami <sayadaazami@gmail.com>
2017-01-08 16:01:54 +0330

    bugfix, namespace

---
+++ src/Mammutgroup/Database/Connectors/ConnectionFactory.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Mammutgroup\Database\Connectors;
+
+use PDO;
+use Illuminate\Database\MySqlConnection;
+use Illuminate\Database\SQLiteConnection;
+use Illuminate\Database\SqlServerConnection;
+use Mammutgroup\Database\MysqlConnection as MyMysqlConnection;
+
+/**
+ * Class ConnectionFactory
+ * @package Mammutgroup\Database\Connectors
+ */
+class ConnectionFactory extends \Illuminate\Database\Connectors\ConnectionFactory
+{
+    /**
+     * @param string $driver
+     * @param PDO $connection
+     * @param string $database
+     * @param string $prefix
+     * @param array $config
+     * @return MySqlConnection|MySqlConnection|SQLiteConnection|SqlServerConnection|mixed|object
+     * @throws \InvalidArgumentException
+     */
+    protected function createConnection($driver, $connection, $database, $prefix = '', array $config = array())
+    {
+//        if ($this->container->bound($key = "db.connection.{$driver}")) {
+//            return $this->container->make($key, array($connection, $database, $prefix, $config));
+//        }
+
+        if ($driver === 'mysql') {
+            return new MyMysqlConnection($connection, $database, $prefix, $config);
+        }
+
+        return parent::createConnection($driver, $connection, $database, $prefix, $config);
+    }
+}
