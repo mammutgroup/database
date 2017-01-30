3e658d5a53a1d42644816e967f40295f85c987ec
MohammadReza Honarkhah <m.honar@gmail.com>
2017-01-30 18:56:41 +0330

    fix issue on point LngLat

--- src/Mammutgroup/Database/Geometries/Factory.php
+++ src/Mammutgroup/Database/Geometries/Factory.php
@@ -3,7 +3,7 @@
 class Factory implements \GeoIO\Factory {
     public function createPoint( $dimension, array $coordinates, $srid = null )
     {
-        return new Point( $coordinates['y'], $coordinates['x'] );
+        return new Point( $coordinates['x'], $coordinates['y'] );
     }
 
     public function createLineString( $dimension, array $points, $srid = null )
